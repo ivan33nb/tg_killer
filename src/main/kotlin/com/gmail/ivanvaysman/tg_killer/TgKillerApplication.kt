c54cca95588d2e1617637264f726f0de6bdeb5a1
out-stroki-waisman <k5LpLsM5Kgjy>
2022-12-19 22:41:16 +0300

    Initial commit

---
+++ src/main/kotlin/com/gmail/ivanvaysman/tg_killer/TgKillerApplication.kt
@@ -0,0 +1,11 @@
+package com.gmail.ivanvaysman.tg_killer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TgKillerApplication
+
+fun main(args: Array<String>) {
+    runApplication<TgKillerApplication>(*args)
+}
---
+++ src/test/kotlin/com/gmail/ivanvaysman/tg_killer/TgKillerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gmail.ivanvaysman.tg_killer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TgKillerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
